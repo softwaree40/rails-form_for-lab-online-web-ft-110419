3ede5453eb8fb2b5e80d9aa85de514d300bafccd
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-02 23:49:28 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,8 +12,8 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
-    @school_class.save
+    @school_class = SchoolClass.create(school_class_params(:title, :room_number))
+    
 
     redirect_to school_class_path(@school_class)
   end
